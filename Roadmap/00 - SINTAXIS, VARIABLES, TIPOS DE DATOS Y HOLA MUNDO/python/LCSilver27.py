b9a8b8aa7771ae8f277d7fce8c6675e28c21b237
LCSilver27 <og7471@gmail.com>
2024-12-31 18:48:39 -0600

    LCSilver27.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/LCSilver27.py
@@ -0,0 +1,29 @@
+#Esta es la URL del lenguaje de programación que quiero aprender: https://python.org/
+
+"Para agrefar comentarios en una sola línea"
+
+"""
+Esta sintaxis se 
+utiliza para agregar
+ comentarios en 
+ varias lineas
+"""
+
+"Para cear una variable"
+variable_1 = "Esta es mi primer variable"
+
+"Para cambiar el valor dela variable"
+variable_1 = "Cambiar el valor a la variable"
+
+"Representación de una contante"
+CONSTANTE_1 = "Contasnte"
+
+"Datos primitivos"
+my_int = 1
+my_float = 10.6
+my_bool = True
+my_bool = False
+my_string = "Hola"
+
+"Imprimir en la terminal"
+print("¡Hola, python!")
